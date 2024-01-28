a825559c25bab34fcb45e9382b6df7a8254ccd7e
Zayna Wasma <126897499+heyitszayna@users.noreply.github.com>
2024-01-28 13:42:17 +0400

    Update PriorityServices.java

--- src/airlinecheckinsystem/PriorityServices.java
+++ src/airlinecheckinsystem/PriorityServices.java
@@ -1,17 +1,12 @@
-package airlinecheckinsystem_sec51_g4;
+package airlinecheckinsystem;
 import java.util.*;
 
-/*
- * CSC301 Assignment 2
- * Done by: Zayna Wasma (1084503)
- */ 
-
-public class PriorityServices_Sec51_G4 {
+public class PriorityServices {
     private String passengerId;
     private int priorityScore;
 
     // parameterized constructor
-    public PriorityServices_Sec51_G4(String passengerId) {
+    public PriorityServices(String passengerId) {
         this.passengerId = passengerId;
         this.priorityScore = 0;
     }
@@ -22,7 +17,7 @@ public class PriorityServices_Sec51_G4 {
     public int getPriorityScore() { return priorityScore; }
     public void setPriorityScore(int priorityScore) { this.priorityScore = priorityScore; }
     
-    public void calculatePriority(Passenger_Sec51_G4 passenger) {
+    public void calculatePriority(Passenger passenger) {
         int score = 0;
         
         switch (passenger.getTicketClass()) {
